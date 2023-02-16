61dcb7a3d8e5c565c00547d8e09a0649ada62b65
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-02-16 14:08:32 +0800

    update the ui connect to api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    alert(`${username} ${password}`);
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+
+        <button>Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    await fetch("http://127.0.0.1:8000/api/create-user", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      contentType: "application/json",
+    });
+  };
+
+  return (
+    <>
+      <h1>register</h1>
+      <form onSubmit={register}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+
+        <button>register</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
